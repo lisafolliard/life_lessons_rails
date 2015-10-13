4dcd8e4cc917cf53ed9601738d52793224eebe56
Yelena Nedelko & Lisa Folliard <student@epicodus.com>
2015-10-13 14:35:45 -0700

    add sections model

---
+++ db/migrate/20151013213355_create_sections.rb
@@ -0,0 +1,10 @@
+class CreateSections < ActiveRecord::Migration
+  def change
+    create_table :sections do |t|
+
+      t.column :title, :string
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151013163317) do
+ActiveRecord::Schema.define(version: 20151013213355) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 20151013163317) do
     t.string   "name"
     t.integer  "number"
     t.string   "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "sections", force: :cascade do |t|
+    t.string   "title"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
