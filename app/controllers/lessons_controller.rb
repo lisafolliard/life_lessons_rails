c37fc7a0dbbefa2aa77d7d5eb2af4803f3e12738
Yelena Nedelko & Lisa Folliard <student@epicodus.com>
2015-10-13 10:52:30 -0700

    retrieve edit form and update lesson

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -20,6 +20,19 @@ class LessonsController < ApplicationController
     end
   end
 
+  def edit
+    @lesson = Lesson.find(params[:id])
+  end
+
+  def update
+    @lesson = Lesson.find(params[:id])
+      if @lesson.update(lesson_params)
+        redirect_to lesson_path
+      else
+        render :edit
+      end
+  end
+
   private
   def lesson_params
     params.require(:lesson).permit(:name, :content, :number)
