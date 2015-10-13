9f0c1fa50a3268074ae571d2868dbf4bf8e5a358
Yelena Nedelko & Lisa Folliard <student@epicodus.com>
2015-10-13 16:14:39 -0700

    add new lesson to section

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,42 +1,40 @@
 class LessonsController < ApplicationController
-  def index
-    @lessons = Lesson.all
-  end
-
-  def show
-    @lesson = Lesson.find(params[:section_id, :id])
-  end
 
   def new
-    @lesson = Lesson.new
+    @section =Section.find(params[:section_id])
+    @lesson = @section.lessons.new
   end
 
   def create
-    @lesson = Lesson.new(lesson_params)
+    @section = Section.find(params[:section_id])
+    @lesson = @section.lessons.new(lesson_params)
     if @lesson.save
-      redirect_to lessons_path
+      redirect_to section_path(@lesson.section)
     else
       render :new
     end
   end
 
   def edit
-    @lesson = Lesson.find(params[:id])
+    @section = Section.find(params[:section_id])
+    @lesson = @section.lessons.find(params[:id])
   end
 
   def update
-    @lesson = Lesson.find(params[:id])
+    @section = Section.find(params[:section_id])
+    @lesson = @section.lessons.find(params[:id])
       if @lesson.update(lesson_params)
-        redirect_to lesson_path
+        redirect_to section_path(@section)
       else
         render :edit
       end
   end
 
   def destroy
-    @lesson = Lesson.find(params[:id])
+    @section = Section.find(params[:section_id])
+    @lesson = @section.lessons.find(params[:id])
     @lesson.destroy
-    redirect_to lessons_path
+    redirect_to section_path(@section)
   end
 
   private
