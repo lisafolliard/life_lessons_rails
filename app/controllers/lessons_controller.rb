68e0687e2e60899f83886a059c8af6d208ce310e
Yelena Nedelko & Lisa Folliard <student@epicodus.com>
2015-10-13 11:07:47 -0700

    delete individual lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -33,6 +33,12 @@ class LessonsController < ApplicationController
       end
   end
 
+  def destroy
+    @lesson = Lesson.find(params[:id])
+    @lesson.destroy
+    redirect_to lessons_path
+  end
+
   private
   def lesson_params
     params.require(:lesson).permit(:name, :content, :number)
