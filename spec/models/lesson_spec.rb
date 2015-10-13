ef0263a924f8b44981d6efab15285bdf4aade6bc
Yelena Nedelko & Lisa Folliard <student@epicodus.com>
2015-10-13 09:49:04 -0700

    add validation for the presence of a lesson number

--- spec/models/lesson_spec.rb
+++ spec/models/lesson_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 describe Lesson do
   it { should validate_presence_of :name }
   it { should validate_presence_of :content }
+  it { should validate_presence_of :number }
 
 end
